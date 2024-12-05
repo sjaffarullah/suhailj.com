5363e16e653e827f8e6027967c2a297060ed455f
125998211-myseneca <sjaffarullah@myseneca.ca>
2024-12-05 17:50:01 -0500

    Edited the experience

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,7 +1,7 @@
 export const EXPERIENCES = [
     {
       year: "May 2023 - Jan 2024",
-      role: "IT Support Analyst Co-op",
+      role: "IT Helpdesk",
       company: "FGF Brands Inc",
       description: "Provided technical support to end-users, resolving hardware, software, and network issues while managing user accounts and access permissions. Maintained IT documentation, oversaw inventory, and ensured timely resolution of incidents within SLAs, contributing to seamless IT operations.",
       technologies: ["Active Directory", "Microsoft Office 365", "ServiceNow ", "SCCM", "Azure" ],
